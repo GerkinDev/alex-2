15b2540350cde7dee15ff632a8cc22c4504a9df0
Gerkin <nihilivin@gmail.com>
2018-01-14 20:43:55 +0100

    Changed footer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,19 +2,26 @@
 
 namespace App\Entity;
 
+use FOSUserBundleModelUser as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User extends BaseUser
 {
-    /**
+	/**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+	protected $id;
 
-    // add your own fields
+	// add your own fields
+
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
 }
