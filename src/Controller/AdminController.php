998ac8ecd110735bf8445dfde9ba14d02ea96791
Unknown <a.ffcc7@gmail.com>
2018-01-17 14:56:04 +0100

    Added content in AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,12 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\AccessMap;
+use Symfony\Component\HttpFoundation\RequestMatcher;
+use Symfony\Component\Security\Http\Firewall\AccessListener;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 
 use App\Entity\Admin;
 
@@ -13,8 +19,29 @@ class AdminController extends Controller
 	/**
 	 * @Route("/admin", name="admin")
 	 */
-	public function index() {
+	public function listener() {
 		// replace this line with your own code!
-		return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+		$accessMap = new AccessMap();
+		$requestMatcher = new RequestMatcher('^/admin');
+		$accessMap->add($requestMatcher, array('ROLE_ADMIN'));
+
+		$accessListener = new AccessListener(
+		    $securityContext,
+		    $accessDecisionManager,
+		    $accessMap,
+		    $authenticationManager
+		);
+	}
+
+	public function checker() {
+		$authorizationChecker = new AuthorizationChecker(
+		    $tokenStorage,
+		    $authenticationManager,
+		    $accessDecisionManager
+		);
+
+		if (!$authorizationChecker->isGranted('ROLE_ADMIN')) {
+		    throw new AccessDeniedException();
+		}
 	}
 }
