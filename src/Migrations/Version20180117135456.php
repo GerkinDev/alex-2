d01843b368d84e6d47ba98855e5dd0aa19728515
Gerkin <nihilivin@gmail.com>
2018-01-21 08:51:44 +0100

    Removed JSON from mysql migrate

--- src/Migrations/Version20180117135456.php
+++ src/Migrations/Version20180117135456.php
@@ -15,7 +15,7 @@ class Version20180117135456 extends AbstractMigration
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
 		$this->addSql('CREATE TABLE material (id INT AUTO_INCREMENT NOT NULL, price DOUBLE PRECISION NOT NULL, available TINYINT(1) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-		$this->addSql('ALTER TABLE model ADD images VARCHAR(255) NOT NULL, CHANGE price price JSON NOT NULL');
+		$this->addSql('ALTER TABLE model ADD images VARCHAR(255) NOT NULL, CHANGE price price TEXT NOT NULL');
 	}
 
 	public function down(Schema $schema) {
