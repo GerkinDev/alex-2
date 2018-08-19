27be4a94d78d5f4bd20e0cf1aac25eae3e75bc23
Gerkin <nihilivin@gmail.com>
2018-08-19 04:43:12 +0200

    Working thing view on print-your-own

--- assets/scripts/components/vue.ts
+++ assets/scripts/components/vue.ts
@@ -2,10 +2,19 @@ import { FlashModule } from './flashes/flashModule';
 import { FlashType } from './flashes/types';
 import Vue from 'vue'
 import Component from 'vue-class-component'
+const { ModelObj, ModelStl } = require( 'vue-3d-model' );
 
 
 export {FlashType};
 export const flashes = new FlashModule();
 flashes.removeAlert({message: 'test', type: FlashType.error});
+
+const vue = new Vue({
+	el: '#object-view',
+	components:{
+		modelObj: ModelObj,
+		modelStl: ModelStl,
+	}
+});
 
 (window as any).flashes = flashes;
